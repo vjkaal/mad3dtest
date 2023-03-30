a3cad6a013db215a5e0b04cb10dbff9a7900d2e4
vjkaal <vanshajkaushal@yahoo.com>
2023-03-30 12:13:45 +0530

    architecture page design underway card half finished

---
+++ js/card.js
@@ -0,0 +1,5 @@
+function toggleCardText(i){
+  let text = document.querySelector('.card-text-container');
+  if(text.style.display === 'none') text.style.display = 'block';
+  else text.style.display = 'none';
+}
