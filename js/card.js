a52c8c7c5681b3fdbdbc8f4552ad65a326b91c6c
vjkaal <vanshajkaushal@yahoo.com>
2023-03-30 12:59:14 +0530

    architecture page css fixed

--- js/card.js
+++ js/card.js
@@ -1,5 +1,6 @@
 function toggleCardText(i){
-  let text = document.querySelector('.card-text-container');
+  let text = document.querySelector('#card-text'+i);
+  // console.log(text.style.display);
   if(text.style.display === 'none') text.style.display = 'block';
   else text.style.display = 'none';
 }
