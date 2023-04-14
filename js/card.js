0b7dcff145783e83d4a7625e7c375541a45a7a35
vjkaal <vanshajkaushal@yahoo.com>
2023-04-15 04:26:27 +0530

    fixed issue with text toggle in desktop mode

--- js/card.js
+++ js/card.js
@@ -1,6 +1,8 @@
 function toggleCardText(i){
-  let text = document.querySelector('#card-text'+i);
-  // console.log(text.style.display);
-  if(text.style.display === 'none') text.style.display = 'block';
-  else text.style.display = 'none';
+  if(window.innerWidth < '960'){
+    let text = document.querySelector('#card-text'+i);
+    // console.log(text.style.display);
+    if(text.style.display === 'none') text.style.display = 'block';
+    else text.style.display = 'none';
+  }
 }
